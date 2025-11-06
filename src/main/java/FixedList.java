8b7926a7eee6e4edce7d1ea854e196eb89f86644
eriinys <eriinys@duck.com>
2025-11-06 10:20:02 -0600

    created generic class and instantiated on main

--- src/main/java/FixedList.java
+++ src/main/java/FixedList.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FixedList {
+public class FixedList<T> {
+    private List<T> items;
+    private int maxSize;
+
+    public FixedList(int maxSize){
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(T item){
+        if (items.size() >= maxSize){
+            System.out.println("Reached the maximum size limit.");
+        } else {
+            items.add(item);
+        }
+    }
+
+    public List<T> getItems(){
+        return items;
+    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
+    public static void main(String[] args) {
+        FixedList<String> names = new FixedList<String>(5);
+
+        names.add("Olivia");
+        names.add("Dave");
+        names.add("Jennifer");
+        names.add("Glynis");
+        names.add("Cynthia");
+        names.add("Alex");
+
+        System.out.println("Maximum size: " + names.getItems().size());
+        System.out.println("Current Members: " + names.getItems());
+
+        FixedList<LocalDate> dates = new FixedList<>(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy");
+
+        dates.add(LocalDate.now());
+        dates.add(LocalDate.now().withDayOfMonth(1));
+        dates.add(LocalDate.now().withDayOfYear(300));
+
+        //format print
+        System.out.println("\nDates stored:");
+        for (LocalDate date : dates.getItems()){
+            System.out.println(date.format(formatter));
+        }
+
+        //normal print with automatic toString formatting
+        System.out.printf("Dates: %s", dates.getItems());
+    }
 }
